b70999a51d5060891ef175bbbef34b0349e5ab82
Ivanmatv <feast0075@gmail.com>
2023-04-19 10:40:17 +0300

    first commit

---
+++ backend/users/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+# from django.db.models import UniqueConstraint
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Адрес электронной почты',
+        unique=True,
+    )
+    username = models.CharField(
+        validators=(UnicodeUsernameValidator(),),
+        verbose_name='Имя пользователя',
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="following"
+    )
+
+    class Meta:
+        # ordering = ('-id', )
+        # constraints = [
+        #     UniqueConstraint(
+        #         fields=('user', 'author'),
+        #         name='unique_follow'
+        #     )
+        # ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self) -> str:
+        return f"{self.user} подписан на {self.author}"
